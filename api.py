95751d59df7e7ad0ac84b99dbff8aae2e3b16b57
Drew Hontz <athontz@gmail.com>
2016-10-19 18:58:59 -0700

    almost all up and running for create user. Just need to work on filter

--- api.py
+++ api.py
@@ -3,14 +3,8 @@
 
 import endpoints
 from protorpc import remote, messages
-from models import User, StringMessage
+from model import User, StringMessage
 
-NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
-GET_GAME_REQUEST = endpoints.ResourceContainer(
-        urlsafe_game_key=messages.StringField(1),)
-GUESS_REQUEST = endpoints.ResourceContainer(
-    MakeMoveForm,
-    urlsafe_game_key=messages.StringField(1),)
 USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
                                            email_address=messages.StringField(2))
 
@@ -25,8 +19,10 @@ class HangmanAPI(remote.Service):
         name='create_new_user'
     )
     def create_user(self, request):
-        if User.query(User.name == request.user_name).get():
-            raise endpointsConflictException("Try again with a new handle, that one is taken.")
+        if User.query(User.user_name == request.user_name).get():
+            raise endpoints.ConflictException("Try again with a new handle, that one is taken.")
         user = User(user_name = request.user_name, email_address = request.email_address)
         user.put()
         return StringMessage(message="Welcome to Hangman, {}".format(request.user_name))
+
+api = endpoints.api_server([HangmanAPI])
